9aa12c0f71b9645bede20a157dbb0bea3bdf9c6f
Vid <vidhan01@easv365.dk>
2026-09-26 17:29:18 +0200

    navigation done
    nav done , added fonts some bad some decent some worse but they are here

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/LogIn">Log In</RouterLink>
-        <RouterLink to="/CharatherMaker">Make your Characther</RouterLink>
+      <nav class="bg-dark text-white gap-5 flex min-h-12.5 items-center px-5 uppercase weight-bold text-xl">
+        <RouterLink to="/" class="hover:text-blue hover:underline ">Home</RouterLink>
+        <RouterLink to="/LogIn" class="hover:text-blue hover:underline">Log In</RouterLink>
+        <RouterLink to="/CharatherMaker" class="hover:text-blue hover:underline">Make your character</RouterLink>
       </nav>
     </div>
   </header>
